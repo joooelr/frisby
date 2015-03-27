3668da5861cb2e6616a0ce32e6d49c6c4f8f2236
Reynaldo <diabloco2000@yahoo.com.ar>
2015-03-27 14:39:11 -0400

    added a new test for authenticated user

--- Reynaldo Guzman/userCrudSpec.js
+++ Reynaldo Guzman/userCrudSpec.js
@@ -21,33 +21,41 @@ frisby.create('Adding user')
 	.inspectJSON()
 	.afterJSON(function(json){
 		var usr = json.Email;
-		console.log('Getting user: '+usr);
-		frisby.create('Getting user created previously')
-		.get('https://todo.ly/api/user.json')
-		.inspectJSON()
-		.afterJSON(function(jsonData){
-			console.log('Updating user: '+jsonData.FullName);
-			frisby.create('Updating user')
-			.put('https://todo.ly/api/user/0.json',{"FullName" : "FullName Updated"},{json: true})
+		frisby.create("Verify if new user is authenticated")
+			.get('https://todo.ly/api/authentication/isauthenticated.json')
 			.inspectJSON()
-			.afterJSON(function(dataUser){
-				console.log('Deleting user');
-				frisby.create('Delete user')
-					.delete('https://todo.ly/api/user/0.json')
-					.inspectJSON()
-					.afterJSON(function(deletedUser){
-					console.log(deletedUser.Email);
-						frisby.create('Deleting user')
-							.delete('https://todo.ly/api/user/0.json')
-							.expectJSON({ErrorCode : 105})
-							.expectJSON({ErrorMessage: "Account doesn't exist"})
-							.inspectJSON()
-						.toss();
-					})
-				.toss();
+			.afterJSON(function(jsons){
+			expect(jsons).toEqual(true);
+			console.log("User is authenticated, true!!");
 			})
 			.toss();
-			})
+			console.log('Getting user: '+usr);
+			frisby.create('Getting user created previously')
+				.get('https://todo.ly/api/user.json')
+				.inspectJSON()
+				.afterJSON(function(jsonData){
+					console.log('Updating user: '+jsonData.FullName);
+					frisby.create('Updating user')
+						.put('https://todo.ly/api/user/0.json',{"FullName" : "FullName Updated"},{json: true})
+						.inspectJSON()
+						.afterJSON(function(dataUser){
+							console.log('Deleting user');
+							frisby.create('Delete user')
+								.delete('https://todo.ly/api/user/0.json')
+								.inspectJSON()
+								.afterJSON(function(deletedUser){
+								console.log(deletedUser.Email);
+									frisby.create('Deleting user')
+										.delete('https://todo.ly/api/user/0.json')
+										.expectJSON({ErrorCode : 105})
+										.expectJSON({ErrorMessage: "Account doesn't exist"})
+										.inspectJSON()
+									.toss();
+								})
+							.toss();
+						})
+					.toss();
+				})
 		.toss();
 	})
 .toss();
